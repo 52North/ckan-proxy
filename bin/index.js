bd0af2109c433c31df94e2b5472da2faac7c4485
Christian Autermann <c.autermann@52north.org>
2015-04-10 13:58:18 +0200

    externalized configuration

--- bin/index.js
+++ bin/index.js
@@ -1,64 +1,54 @@
 #!/usr/bin/env node
-var log = require('../lib/logging')('main');
-var Proxy = require('../lib/proxy');
+
 var cluster = require('cluster');
 var hub = require('clusterhub');
-//var WhitelistManager = require('../lib/whitelist').Manager;
-var Whitelist = require('../lib/whitelist').Whitelist;
-var WhitelistManager = require('../lib/whitelist-dummy').Manager;
+var os = require('os');
 
-var domains = [
-  'localhost',
-  'jamaika',
-  'zoidberg',
-  'requestb.in',
-  '127.0.0.1',
-  'sensorweb.demo.52north.org',
-  'google.com',
-  'facebook.com',
-  'amazon.com',
-  'twitter.com',
-  'github.com',
-  'cdnjs.cloudflare.com',
-  'maxcdn.bootstrapcdn.com',
-  'code.jquery.com',
-  'ajax.googleapis.com'
-];
+var log = require('../lib/logging')('main');
+var config = require('../lib/config');
+var Whitelist = require('../lib/whitelist');
+var Proxy = require('../lib/proxy');
 
 if (cluster.isMaster) {
+  createMaster();
+} else {
+  createWorker();
+}
 
-  WhitelistManager.create({
-    url: 'http://demo.ckan.org',
-    updateInterval: 0,
-    rowsPerRequest: 500,
-    domains: domains
-  }).on('update', function(whitelist) {
-    var newdomains = whitelist.get();
-    Array.prototype.push.apply(newdomains, domains);
-    log.debug('[MASTER] Whitelist changed.');
-    hub.emit('whitelist.update', newdomains);
-  });
+function createMaster() {
+  var cpus = os.cpus().length;
 
-  var cpus = require('os').cpus().length;
-  for (var i = 0; i < cpus; ++i) {
-    cluster.fork();
-  }
+  Whitelist.Manager.create(config.whitelist)
+      .on('update', function(whitelist) {
+        var newdomains = whitelist.get();
+        // maintain additional domains across updates
+        log.debug('[MASTER] Whitelist changed.');
+        // send the whitelist to the workers
+        hub.emit('whitelist.update', newdomains);
+      });
 
   cluster.on('exit', function(worker, code, signal) {
     log.info('worker ' + worker.process.pid + ' died');
+    // restart a work if it wasn't suicide
     if (!worker.suicide) cluster.fork();
   });
 
-} else {
+  for (var i = 0; i < cpus; ++i) {
+    cluster.fork();
+  }
+}
 
-  var proxy = Proxy.create({});
-  proxy.whitelist.set(domains);
-
+function createWorker() {
+  var proxy = new Proxy(config.proxy);
+  // set up the initial whitelist
+  proxy.whitelist.set(config.whitelist.domains);
+  // listen for whitelist changes
   hub.on('whitelist.update', function(whitelist) {
-    log.debug('[WORKER] Setting changed.');
+    log.debug('[WORKER] Whitelist changed.');
     proxy.whitelist.set(whitelist);
   });
+  log.info("Starting server on port " + config.proxy.port);
 
-  log.info("Starting server on port 9090");
-  proxy.server.listen(9090);
+  // start the server
+  proxy.start();
 }
